a27060f932affc3fbe8f69afdaf71a16370dfef7
Randolfo <ragnifico@yahoo.com.br>
2024-03-14 13:49:24 -0300

    Program tar 01

--- Examples/Tarefa_01/Main (conflict 2024-03-14-13-33-48).java
+++ Examples/Tarefa_01/Main (conflict 2024-03-14-13-33-48).java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class Main{
     // Obj para ler entrada:
     private static Scanner read = new Scanner(System.in);
+    private static int cont=-1;
 
     // Class de somatorio:
-    static class Ex_01{ float add(float n){ return ((n=read.nextFloat())>=0)?n+add(n):0; }};
+    static class Ex_01{ float add(float n){++cont; return ((n=read.nextFloat())>=0)?n+add(n):0; }};
     
     // Class de media:
-    static class Ex_02{ float average(int cnt,int smt){ return (new Ex_01().add(0)/smt); }};
+    static class Ex_02{ float average(){ cont=-1; return (new Ex_01().add(0))/cont;}};
     
     // Class de  acreimo salarial:
-    static class Ex_03{ static float salary(float money){ return money+(money/4); }};    
+    static class Ex_03{ float salary(float money,float acr){ return money+(money*(acr/100)); }};    
 
     // Program: 
     static class Program{
+        private 
+        String space(){ return "\n____________________________\n";}
+
+        private 
         int Layout(){
             System.out.println(
                 " Tarefa 01\n"+
@@ -26,34 +31,30 @@ public class Main{
             );
             return read.nextInt();
         }
+        
         void show(){
             switch (Layout()){
                 case 0: return;
                 case 1: {
                     System.out.println("Dígite -1 para sair:"); 
-                    System.out.println("Soma = "+(new Ex_01().add(0)));
+                    System.out.println("Soma = "+(new Ex_01().add(0))+space());
                 }break;
                 case 2:{
                     System.out.println("Dígite -1 para sair:"); 
-                    System.out.println("Soma = "+(new Ex_01().add(0)));
+                    System.out.println("Média = "+(new Ex_02().average())+space());
                 }break;
                 case 3:{
-
+                    System.out.println("Quanto você recebe e o acrescimo: ");
+                    System.out.println("Total = R$ "+
+                            (new Ex_03().salary(
+                                read.nextFloat(), /* Salário     */
+                                read.nextInt()    /* Porcentagem */
+                            )+space()));
                 }break;
             }show();
         }
         
      }
 
-    public static void main(String[] args) {
-        new Program().show();
-
-        /*if(args[1]=="somar"){  
-            System.out.println("Dígite -1 para encerrar somas:"); 
-            System.out.println("Somatório = "+(new Ex_01().add(0))); 
-       }*/
-       //System.out.println("Dígite suas 3 notas semestrais"); 
-       //System.out.println("Média = "+(new Ex_02().average(3, 3))); 
-       //System.out.println("Um salario 1200 + 25% = "+(new Ex_03().salary(1200))); 
-    }
+    public static void main(String[] args) { new Program().show(); }
 }
